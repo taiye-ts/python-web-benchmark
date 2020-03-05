a3f258a5ceece4efba9f293b41ea8c93a0df35dc
tstaiye <ts.taiye@gmail.com>
2020-03-05 14:53:32 +0300

    added content to body

--- bench.lua
+++ bench.lua
@@ -1,3 +1,3 @@
 wrk.method = "POST"
-wrk.body   = '{"test": "42"}'
+wrk.body   = '{"transaction_id": "42", "some_version": "0", "transactions": [{"transaction_type_id": "142", "balance": "24"}]}'
 wrk.headers["Content-Type"] = "application/json"
